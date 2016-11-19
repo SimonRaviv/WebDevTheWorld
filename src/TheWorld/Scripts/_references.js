a386dd22ae836c16b663d8003008e4aae5551a54
Simon <lionsimonlion@gmail.com>
2016-11-19 23:57:32 +0200

    Initial commit, the project until module 3 in Plurasight.

---
+++ src/TheWorld/Scripts/_references.js
@@ -0,0 +1,4 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="../wwwroot/js/site.js" />
+/// <reference path="../wwwroot/lib/jQuery/dist/jquery.js" />
+/// <reference path="../wwwroot/lib/underscore/underscore.js" />
---
+++ src/TheWorld/wwwroot/js/site.js
@@ -0,0 +1,32 @@
+﻿/*site.js*/
+
+(function () {
+
+	//var user_name_element = $("#user_name");
+	//user_name_element.text("Simon Raviv");
+
+	//var main = $("#main");
+	//main.on("mouseenter", function () {
+	//	main.style = "background-color: #888;";
+	//});
+
+	//main.on("mouseleave", function () {
+	//	main.style = "";
+	//});
+
+	//var menu_items = $("ul.menu li a");
+	//menu_items.on("click", function () {
+	//	var clicked_item = $(this);
+	//	alert(clicked_item.text());
+	//});
+
+	var $sidebar_and_wrraper = $("#sidebar,#wrapper");
+
+	$("#sidebar_toggle").on("click", function () {
+		$sidebar_and_wrraper.toggleClass("hide-sidebar");
+		if ($sidebar_and_wrraper.hasClass("hide-sidebar"))
+			$(this).text("Show sidebar");
+		else
+			$(this).text("Hide sidebar");
+	});
+})();
