120eb6ccd83031fe7aa901a4c05e67bf1c9ab464
Simon <lionsimonlion@gmail.com>
2016-11-28 23:44:15 +0200

    MVC 6 module support

---
+++ src/TheWorld/Controllers/Web/AppController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TheWorld.ViewModels;
+using TheWorld.Services;
+using Microsoft.Extensions.Configuration;
+
+namespace TheWorld.Controllers.Web.App
+{
+	public class AppController : Controller
+	{
+		private IMailService _mailService;
+		private IConfigurationRoot _config;
+
+		public AppController(IMailService mailService, IConfigurationRoot config)
+		{
+			_mailService = mailService;
+			_config = config;
+		}
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Contact()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Contact(ContectViewModel model)
+		{
+			if (ModelState.IsValid)
+			{
+				_mailService.SendMail(_config["MailSettings:ToAddress"], model.Email, "From TheWorld", model.Message);
+				ModelState.Clear();
+				ViewBag.UserMessage = "Message Sent";
+			}
+			return View();
+		}
+
+		public IActionResult About()
+		{
+			return View();
+		}
+	}
+}
--- src/TheWorld/Scripts/_references.js
+++ src/TheWorld/Scripts/_references.js
@@ -1,4 +1,6 @@
 ﻿/// <autosync enabled="true" />
 /// <reference path="../wwwroot/js/site.js" />
 /// <reference path="../wwwroot/lib/jQuery/dist/jquery.js" />
+/// <reference path="../wwwroot/lib/jquery-validation/dist/jquery.validate.js" />
+/// <reference path="../wwwroot/lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js" />
 /// <reference path="../wwwroot/lib/underscore/underscore.js" />
